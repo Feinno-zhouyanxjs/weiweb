ae4e4cad5d9e8a1ce6121847dbea91885c928a83
sunny <benben123456@163.com>
2014-01-18 18:18:52 +0800

    添加日志

--- src/main/java/com/sunny/weiweb/controller/CoreController.java
+++ src/main/java/com/sunny/weiweb/controller/CoreController.java
@@ -3,10 +3,11 @@ package com.sunny.weiweb.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sunny.weiweb.utils.SignUtil;
@@ -14,9 +15,20 @@ import com.sunny.weiweb.utils.SignUtil;
 @Controller
 public class CoreController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@ResponseBody
 	@RequestMapping(value = "/Core.do", method = { RequestMethod.POST, RequestMethod.GET })
-	public String getString(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr) {
+	public String getString(HttpServletRequest request, HttpServletResponse response) {
+		// 微信加密签名
+		String signature = request.getParameter("signature");
+		// 时间戳
+		String timestamp = request.getParameter("timestamp");
+		// 随机数
+		String nonce = request.getParameter("nonce");
+		// 随机字符串
+		String echostr = request.getParameter("echostr");
+		logger.debug(signature + "-" + timestamp + "-" + nonce + "-" + echostr);
 		if (SignUtil.checkSignature(signature, timestamp, nonce)) {
 			return echostr;
 		}
