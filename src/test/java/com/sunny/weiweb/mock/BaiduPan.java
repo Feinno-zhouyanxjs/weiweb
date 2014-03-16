662f04eb1d332fc7b69294fe839e59794c564648
xiaxiayoyo <xiaxiayoyo@gmail.com>
2014-03-16 17:36:05 +0800

    增加EMAIL收发测试代码

---
+++ src/test/java/com/sunny/weiweb/mock/BaiduPan.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.sunny.weiweb.mock;
+
+/**
+ * 
+ *
+ * Create on 2014-3-8 下午5:48:18
+ *
+ * @author <a href="mailto:zhouyan@pzoomtech.com">ZhouYan</a>. 
+ * 
+ */
+public class BaiduPan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
